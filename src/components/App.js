279502a431218b748b7c84a0951d8261bcce9e67
Kartik Arora <kartikarora99@gmail.com>
2019-08-16 11:02:41 -0700

    revert back to vanilla react

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,80 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/App.css';
+import logo from "../image/Logo.png";
 import Header from './Header'
 import Sidebar from './Sidebar'
 import MapWithMarkers from "./Map"
 import ServiceTable from "./ServiceTable"
 import HostTable from "./HostTable"
+import { Row, Col, Container, Dropdown, DropdownButton, Accordion, Card, Button, ListGroup } from 'react-bootstrap'
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
     this.state = {
       results: {}
     }
   }
 
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then((data) => {
+      // this.setState({ results: data })
+      console.log(data)
+    })
+    .catch(console.log)
+  }
+
   render() {
     return (
-      <div className="App"> {/* Parent Div */}
-      
-        <Header /> 
-
-        <Sidebar />
-        <MapWithMarkers />
-        <ServiceTable />
-        <HostTable />
+      <div>
+        <div className="topnav">
+          <img src={logo}></img>
+          <input type="text" placeholder="Search.." />
+          <button className="SearchButton">Search</button>
+        </div>
+        <Container>
+          <Row>
+            <Col >
+              <Accordion>
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                      Communities
+      </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                      <ListGroup>
+                        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                      </ListGroup>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                      Browser
+      </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body>
+                      <ListGroup>
+                        <ListGroup.Item>Empty</ListGroup.Item>
+                      </ListGroup>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </Col>
+            <Col xs={6}>2 of 2</Col>
+          </Row>
+        </Container>
       </div>
     );
   }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,110 @@
 import React, {Component} from 'react';
 import '../styles/Sidebar.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 class Sidebar extends Component{
-  constructor(){
-    super();
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
     this.state = {
-      results: {}
-    }
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
   render() {
     return (
-        <div className= "App-sidebar">
-            <button>Browse</button>
-            <button>Community</button>
-            <button>Deveoper</button>
-        </div>
+      <div className="App-sidebar">
+         <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 3
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 4
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 5
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 6
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+
+        
+      </div>
     );
   }
-
-};
+}
 
 export default Sidebar;
