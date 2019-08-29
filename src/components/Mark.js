5496ba46edb582415a15372c9e8c84da0636c4c9
Kartik Arora <kartikarora99@gmail.com>
2019-08-29 11:52:05 -0700

    add info box

--- src/components/Mark.js
+++ src/components/Mark.js
@@ -2,15 +2,33 @@ import React from "react";
 import { Marker } from "react-google-maps";
 // import StethoscopeIcon from "../stethoscopeIcon.png";
 import dot from "../image/dot.png"
+import {InfoWindow } from 'react-google-maps'
+
 
 export default class Mark extends React.Component {
 
+  constructor(){
+    super();
+    this.state={
+      isOpen: false
+    }
+    this.onToggle = this.onToggle.bind(this);
+  }
+
+ onToggle(){
+  this.setState({isOpen: !this.state.isOpen})
+ }
   render(){
     return(
         <Marker
           position={this.props.location}
-        icon={dot}
+          icon={dot}
+          onClick={this.onToggle}
         >
+          {this.state.isOpen && 
+                <InfoWindow onCloseClick={this.onToggle}>
+                    <div>Open</div>
+                </InfoWindow>}
         </Marker>
     );
   }
